7163862839c2ed1a8117ce4c0f67f7e12b492d79
Rednexie <126991413+Rednexie@users.noreply.github.com>
2024-01-15 22:05:55 +0000

    new file:   .gitignore 	new file:   index.js 	new file:   logs 	new file:   package-lock.json 	new file:   package.json

---
+++ index.js
@@ -0,0 +1,76 @@
+const cors = require('@fastify/cors')
+const fs = require('fs')
+const fastify = require('fastify')({
+    trustProxy: true,
+    logger: false,
+});
+
+
+
+
+
+const log = true;
+const port = 3000;
+
+
+
+
+
+fastify.register(require('@fastify/cors'))
+
+
+fastify.addHook('preHandler', (req, res, next) => {
+    if(log){
+        fs.appendFile('./logs', `[${new Date().toLocaleString()}] ${req.ip}: ${req.method} => ${req.originalUrl}`, (err) => {
+            if(err) console.error(err)
+        })
+    }
+    console.log(`[${new Date().toLocaleString()}] ${req.ip}: ${req.method} => ${req.originalUrl}`)
+    next()
+})
+
+fastify.get('/api', (request, reply) => {
+    const ip = request.ip;
+    return reply.send(ip)
+})
+
+
+fastify.get('/text', {
+    preHandler: (request, reply) => {
+    
+    },
+    handler: (request, reply) => {
+        return request.ip;
+    }
+})
+
+fastify.get('/json', (request, reply) => {
+    const ip = request.ip
+    return reply.send({ ip })
+})
+
+fastify.get('/ips', (request, reply) => {
+    return request.ips;
+})
+
+
+fastify.get('/ip', (request, reply) => {
+    return request.ip
+})
+
+
+process.on('uncaughtException', (error) => {
+    console.error(error)
+    fastify.close();
+    process.exit(1)
+})
+
+fastify.listen({ port }, (err, addr) => {
+    if(err){
+        console.error(err);
+        process.exit(1)
+    }
+    console.log('fastify-ip is listening on port', port)
+})
+
+if(log) fs.writeFileSync('./logs', '', 'utf-8')
